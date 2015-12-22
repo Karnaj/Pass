5578e68bff7ab7e08d2c14cc427f3cb7fbb9a711
Karnaj <yoan.geran@hotmail.fr>
2015-12-22 14:17:57 -0400

    Create src/main.c

---
+++ src/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <conio.h>
+
+void getpass(char pass[], int len);
+
+void getpass(char pass[], int len)
+{
+  int c;
+	int n = 0;
+	do
+	{
+		c = getch();  
+		if(isalnum(c))  /* Si c'est un caractère alphanumérique */
+		{
+		pass[n] = c; /* on l'ajoute au tableau */
+		n++;
+		}
+	}
+	while(c != 0x0D && n < len);    /* jusqu'à entrée ou longueur maximale */
+	pass[n] = 0; /* On rajoute le 0 de fin de chaîne */
+	printf("\n");  /* On revient à la ligne */
+}
+
+int main(void)
+{
+	char a[11];
+	printf("Entrez votre mot de passe (seuls les 10 premiers caracteres seront pris en compte) : ");
+	getpass(a, 10);    
+	printf("Nombre = %s\n",a);
+	return 0;
+}
